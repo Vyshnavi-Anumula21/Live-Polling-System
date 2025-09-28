86678a92eb4a3aeabf269370779d613f00482bee
Vyshnavi Anumula <anumulavyshnavi@gmail.com>
2025-09-28 17:19:40 +0530

    Updated CORS origin and port for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://live-polling-system-frontend-6kur.onrender.com",
     methods: ["GET", "POST"],
   },
 });
